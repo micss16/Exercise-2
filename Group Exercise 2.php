c6a9109a17bae0c53c1d5c4a0d4c77156415e76a
micss16 <micobacolor16@gmail.com>
2024-08-30 00:17:53 +0800

    Add files via upload

---
+++ Group Exercise 2.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <title>Personal Web</title>
+    <link rel="stylesheet" href="Group Exercise 2.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <button id="toggle-button">Toggle Dark/Light Mode</button>
+            <header>
+                <h1>When someone is in need <br>Group 4 is indeed</h1>
+            </header>
+            <div class="container-up">
+                <div class="team">
+                    <div class="team-member">
+                        <img src="pics/sazon.jpg">
+                        <h2>Jean Lyle C. Sazon</h2>
+                        <p><b>Age:</b> 19 years old</p>
+                        <p><b>Hobby:</b> Reading</p>
+                    </div>
+                    
+                    <div class="team-member">
+                        <img src="pics/torres.jpg">
+                        <h2>Isaiah N. Torres</h2>
+                        <p><b>Age:</b> 21 years old</p>
+                        <p><b>Hobby:</b> Playing basketball</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container-down">
+                <div class="team">
+                    <div class="team-member">
+                        <img src="pics/cruz.jpg">
+                        <h2>Gerome M. Cruz</h2>
+                        <p><b>Age:</b> 20 years old</p>
+                        <p><b>Hobby:</b> Do the task properly and hang out with family and friends</p>
+                    </div>
+    
+                    <div class="team-member">
+                        <img src="pics/bandejes.jpg">
+                        <h2>Roderick John Bandejes</h2>
+                        <p><b>Age:</b> 20 years old</p>
+                        <p><b>Hobby:</b> Going to the gym and reading</p>
+                    </div>
+    
+                    <div class="team-member">
+                        <img src="pics/bacolor.jpg">
+                        <h2>Mico L. Bacolor</h2>
+                        <p><b>Age:</b> 20 years old</p>
+                        <p><b>Hobby:</b> photography and pixel art</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h2>About Us</h2>
+        <div class="about">
+           <img src="pics/about-bg1.jpg">
+           <div class="about-desc"> 
+                <p> 
+                Meet Our Talented Coding Students of PLMun
+                <br>
+                1. Isaiah Torres - Web design <br>
+                He is a talented  web designer who blends aesthetics with functionality to create visually stunning and user-centric websites.
+                With a deep understanding of design principles and tools like Adobe XD and Figma, Jordan crafts engaging layouts and intuitive navigation. 
+                focus is on delivering designs that not only look great but also provide a seamless and enjoyable experience for users.
+                <br>
+                2. Jean Lyle Sazon - Javascript<br>
+                He is a dynamic JavaScript developer with a passion for building interactive web applications.
+                Specializing in modern JavaScript frameworks such as React and Vue.js, Alex creates responsive and feature-rich user interfaces.
+                With a keen eye for detail and a commitment to writing clean, efficient code, Alex excels at bringing complex functionalities to life and enhancing user experiences.
+                <br>
+                3. Roderick Bandejes - PHP<br>
+                He is an experienced PHP developer specializing in server-side scripting and dynamic website development. Proficient in PHP frameworks such as Laravel and Symfony, Sam builds robust back-end systems that power complex web applications. 
+                expertise in integrating databases, handling server logic, and ensuring application security makes them a key player in developing scalable and efficient web solutions.
+                <br>
+                4. Gerome Cruz - CSS<br>
+                He is a CSS expert who excels in styling and enhancing the visual appeal of web pages. With extensive knowledge of CSS and pre-processors like SASS, Morgan designs responsive and visually appealing layouts that adapt seamlessly across different devices.
+                ability to implement advanced styling techniques and animations helps create engaging and polished user interfaces.
+                <br>
+                5. Mico Bacolor - HTML<br>
+                He is an HTML specialist with a strong foundation in structuring and organizing web content. Skilled in HTML, Taylor creates semantic and accessible markup that forms the backbone of well-structured web pages. 
+                's attention to detail ensures that content is properly organized and optimized for both search engines and user experience.
+                <br>
+                "In the world of IT, enthusiasm is the spark that ignites innovation and turns challenges into opportunities. 
+                It's not just about solving problems but about embracing the journey with passion and curiosity.".</p>
+           </div>
+        </div>
+        <script src="Group Exercise 2.js"></script>
+    </body>
+</html>
+
+
